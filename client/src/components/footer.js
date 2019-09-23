7f59a4bf03992de597c718b9f303f414fb5c0519
sean <seanvanloggerenberg@gmail.com>
2019-09-23 06:57:49 +0200

    first

---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@2019 Sean VL</span>
+      </div>
+    </footer>
+  );
+}
